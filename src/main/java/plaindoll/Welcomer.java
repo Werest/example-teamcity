6512d0bbc4ac66e630f6350f8aeb07624c15bce5
Werest <33048498+Werest@users.noreply.github.com>
2025-08-02 12:21:43 +0300

    add Hunter + test

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayHunter(){
+		return "I was hunter three years ago, but you don't know that history"
+	}
 }
